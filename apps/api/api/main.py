14623dae528354355d585fb63bf03285d4c05390
gtomas <gtomas@student.42luxembourg.lu>
2026-05-05 17:07:32 +0200

    ran some basic tests aboput scraping

--- apps/api/api/main.py
+++ apps/api/api/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import requests
+from bs4 import BeautifulSoup
+
+def test_soup():
+    html = requests.get("https://www.idealwine.com/fr/prix-vin/133180------Bouteille-Charente-Cognac-Louis-XIII-Remy-Martin-ambre").text
+    soup = BeautifulSoup(html, "html.parser")
+    print(type(soup))
+    return soup
+
+
+
+
 app = FastAPI(title="Chimerai API", version="0.1.0")
 
 app.add_middleware(
@@ -16,7 +28,10 @@ app.add_middleware(
 async def root():
     return {"status": "ok", "message": "Chimerai API is running"}
 
+def create_user():
+    return True
 
 @app.get("/health")
 async def health():
-    return {"status": "healthy"}
+    test_soup()
+    return{"data" : f"{test_soup()}"}
