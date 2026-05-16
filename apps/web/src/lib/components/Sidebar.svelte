ca3bdac0ca8502eed121b1690cae1c17a4a74cf6
samtvn <horned-bacon0@icloud.com>
2026-05-16 22:40:08 +0200

    fix: frontend config

---
+++ apps/web/src/lib/components/Sidebar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { LayoutDashboard } from "@lucide/svelte";
+</script>
+
+<div class="drawer-side">
+  <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"
+  ></label>
+  <ul class="menu bg-base-200 min-h-full w-40 p-4">
+    <!-- Sidebar content here -->
+    <li><a href="/"><LayoutDashboard size="14" /> Dashboard</a></li>
+    <li><a href="/">Sidebar Item 2</a></li>
+  </ul>
+</div>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import Sidebar from "$lib/components/Sidebar.svelte";
+  import { Menu } from "@lucide/svelte";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="drawer lg:drawer-open">
+  <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col">
+    <div class="navbar bg-base-100 lg:hidden">
+      <label for="my-drawer-3" class="btn btn-square btn-ghost">
+        <Menu />
+      </label>
+    </div>
+    <!-- Page content here -->
+    {@render children()}
+  </div>
+  <Sidebar />
+</div>
